300051fd0d9437963ccbcc9c53a0e90b9f0da60d
Gagan Devagiri <gagandevagiri@gmail.com>
2019-01-26 20:50:38 +0000

    Added second draft

---
+++ MyApplication/app/src/main/java/com/hackic/seizure/SensorService.java
@@ -0,0 +1,29 @@
+package com.hackic.seizure;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.os.IBinder;
+
+public class SensorService extends Service implements SensorEventListener {
+    public SensorService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
