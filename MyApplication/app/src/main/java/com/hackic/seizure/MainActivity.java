f625acdd993f1bf03242f2f560958bcef6b81e21
Gagan Devagiri <gagandevagiri@gmail.com>
2019-01-27 01:52:09 +0000

    Completed the design and front - end of the app

--- MyApplication/app/src/main/java/com/hackic/seizure/MainActivity.java
+++ MyApplication/app/src/main/java/com/hackic/seizure/MainActivity.java
@@ -1,47 +1,84 @@
 package com.hackic.seizure;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
-    private int clickCount = 0;
+    private final Runnable updater =  new Runnable() {
+        int startingTime = 15;
+        @Override
+        public void run() {
+
+            if (startingTime <= 0) {
+                MakePhoneCall();
+            }
+            else {
+                TextView t = (TextView) findViewById(R.id.timer);
+                startingTime -= 1;
+                t.setText("" + startingTime);
+            }
+
+        }
+    };
+
+    private final Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                clickCount += 1;
-                TextView textView = findViewById(R.id.textView);
-                textView.setText("You clicked " + clickCount + " times");
-
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                MakePhoneCall();
             }
         });
+
+                Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+        for (int i = 1; i < 20; i++) {
+            handler.postDelayed(updater,i*1000);
+        }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void MakePhoneCall(){
+        Intent callIntent =new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse("tel:000"));
+        startActivity(callIntent);
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -56,4 +93,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
